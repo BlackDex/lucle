300277324be774648cfc2160897fae67fee7b5be
Ludea <ludovicw35@hotmail.com>
2024-06-13 22:01:14 +0200

    Update http.rs

--- src/http.rs
+++ src/http.rs
@@ -11,40 +11,4 @@ pub fn serve_dir() -> Router {
         .nest_service("/", serve_dir.clone())
         .fallback_service(serve_dir)
         .layer(TraceLayer::new_for_http())
-}
-
-/* pub async fn serve_https(app: Router, rustls_config: ServerConfig) {
-    let tls_acceptor = TlsAcceptor::from(Arc::new(rustls_config));
-    let bind = "[::1]:8080";
-    let tcp_listener = TcpListener::bind(bind).await.unwrap();
-    tracing::info!("HTTPS server listening on {}", bind);
-
-    pin_mut!(tcp_listener);
-    loop {
-        let tower_service = app.clone();
-        let tls_acceptor = tls_acceptor.clone();
-
-        let (cnx, addr) = tcp_listener.accept().await.unwrap();
-
-        tokio::spawn(async move {
-            let Ok(stream) = tls_acceptor.accept(cnx).await else {
-                tracing::error!("error during tls handshake connection from {}", addr);
-                return;
-            };
-
-            let stream = TokioIo::new(stream);
-
-            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
-                tower_service.clone().call(request)
-            });
-
-            let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
-                .serve_connection_with_upgrades(stream, hyper_service)
-                .await;
-
-            if let Err(err) = ret {
-                tracing::warn!("error serving connection from {}: {}", addr, err);
-            }
-        });
-    }
-} */
+} 
